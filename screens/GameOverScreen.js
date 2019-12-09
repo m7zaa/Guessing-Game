a1ecadb51290304ccaf82e04dc797f1189ee31e3
Micah Zaayer <micahzaayer@gmail.com>
2019-12-09 15:58:59 -0800

    finish game logic.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -5,6 +5,9 @@ const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>The Game is Over!</Text>
+      <Text>Number of Rounds: {props.roundsNumber}</Text>
+      <Text>Number was: {props.userNumber}</Text>
+      <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   )
 };
